0f58b104741fea89d29dc8a9773191eb2239bde1
Ramires <moreira.ramires@gmail.com>
2022-06-28 11:17:15 -0300

    add logo.svg via componente react

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from './pages/Cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-   <div>
-    Nonna Vera
-   </div>
+    <Cardapio/>
   </React.StrictMode>
 );
 
