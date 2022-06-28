218bc69016f8b31892701a378991af01c60fab93
Ramires <moreira.ramires@gmail.com>
2022-06-28 17:57:44 -0300

    icon buscador'

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Buscador.module.scss';
-
+import {CgSearch} from 'react-icons/cg'
 interface Props {
   busca: string;
   setBusca: React.Dispatch<React.SetStateAction<string>>
@@ -11,8 +11,8 @@ export default function Buscador({busca,setBusca}: Props ){
 
   return(
     <div className={styles.buscador}>
-      <input type="text"  value ={busca} onChange={(event) => setBusca(event.target.value)}/>
-      
+      <input  value ={busca} onChange={(event) => setBusca(event.target.value)}/>
+      <CgSearch size={20} color ='#4c4d5e'/>
     </div>
   )
 
