fc0d4ae3ec504c337345a23fa3483f6a0fd845b5
Ramires <moreira.ramires@gmail.com>
2022-07-01 10:26:02 -0300

    add imgs

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,6 @@
-import logo from 'assets/logo.svg';
 import styles from './Item.module.scss'; 
-import cardapio from '../itens.json'
+import cardapio from '../itens.json';
+import clasNames from 'classnames'
 
 type Props = typeof cardapio[0]
 
@@ -18,7 +18,13 @@ return(
           <p>{description}</p>
       </div>
       <div className={styles.item__tags}>
-        <div className={styles.item__tipo}>
+        <div className={clasNames({
+          [styles.item__tipo]:true,
+          [styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]:true,
+        })
+
+
+        }>
           {category.label}
         </div>
         <div className={styles.item__porcao}>
